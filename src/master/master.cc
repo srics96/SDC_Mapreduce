ec0587f4a0a506b274b104942bc7b05c3c45b6c9
Sricharan <sravi73>
2022-03-18 19:20:26 +0000

    all the changes

--- src/master/master.cc
+++ src/master/master.cc
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <memory>
-#include <string>
-
 #include <zookeeper/zookeeper.h>
 #include <conservator/ConservatorFrameworkFactory.h>
 #include <glog/logging.h>
@@ -11,6 +7,10 @@
 #include <algorithm>
 #include <chrono>
 #include <thread>
+
+#include <iostream>
+#include <memory>
+#include <string>
 
 #include <grpcpp/grpcpp.h>
 
@@ -53,81 +53,23 @@ class MasterClient {
         std::unique_ptr<WorkerService::Stub> stub_;
 };
 
-bool is_master() {
-    ConservatorFrameworkFactory factory = ConservatorFrameworkFactory();
-    
-    std::unique_ptr<ConservatorFramework> framework = factory.newClient("localhost:2181");
-
-    framework->start();
-    LOG(INFO) << "Start ZK framework";
-    {
-        auto res = framework->create()->forPath("/masters");
-        LOG(INFO) << "Create /masters retval:" << res;
-    }
-    {
-        auto res = framework->checkExists()->forPath("/masters");
-        assert(res == ZOK);
-        LOG(INFO) << "/masters now exists";
-    }
-    std::string realpath;
-    {
-    auto res = framework->create()
-                   ->withFlags(ZOO_EPHEMERAL | ZOO_SEQUENCE)
-                   ->forPath("/masters/" + hostname + "_", NULL, realpath);
-    if (res != ZOK) {
-      LOG(FATAL) << "Failed to create ephemeral node, retval "<< res;
-    } else {
-      LOG(INFO) << "Created seq ephm node " << realpath;
-      realpath = realpath.substr(realpath.find_last_of('/') + 1);
-    }
-    }
-    // Check if we are the primary master
-    bool is_main_master = false;
-    while (!is_main_master) {
-        std::vector<std::string> everyone =
-        framework->getChildren()->forPath("/masters");
-        auto ref =
-        std::min_element(everyone.cbegin(), everyone.cend(),
-                         [](const std::string& a, const std::string& b) {
-                           return get_trailing_num(a) < get_trailing_num(b);
-                         });
-        if (*ref == realpath) {
-            LOG(INFO) << "This is the master node";
-            is_main_master = true;
-        } else {
-      // TODO set watch
-        LOG(INFO) << "Waiting as backup master";
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        }
-    }
-
-    if (is_main_master) {
-    std::string worker;
-    std::vector<std::string> workers =
-        framework->getChildren()->forPath("/workers");
-    if (workers.empty()) {
-      LOG(FATAL) << "No workers";
-    }
-    else {
-      worker = workers[0];
-    }
-        return true;
-    }
-  while (true) {
-    LOG(INFO) << "Waiting to be killed (sleeping for 2 seconds)";
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-  }
-}
-
 int main(int argc, char** argv) {
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_stderrthreshold = 0;
+    LOG(INFO) << "The Master has started" << endl;
 
+    ConservatorFrameworkFactory factory = ConservatorFrameworkFactory();
+    std::unique_ptr<ConservatorFramework> framework = factory.newClient("my-release-zookeeper:2181");
+    framework->start();
+
+    LOG(INFO) << "Connected to the zookeeper service" << endl;
+    
     string serverAddress = "worker-service:5001";
-    cout << "The server address is " << serverAddress << endl;
-    is_master();
+    LOG(INFO) << "The server address is " << serverAddress << endl;
     MasterClient masterClient(grpc::CreateChannel(serverAddress, grpc::InsecureChannelCredentials()));
-    cout << "Connected to the server.";
+    LOG(INFO) << "Connected to server" << endl;
     string message(serverAddress);
     string reply = masterClient.handshake(message); 
-    cout << "Handshake response received: " << reply << std::endl;
+    LOG(INFO) << "Handshake response received: " << reply << std::endl;
     return 0;
 }
