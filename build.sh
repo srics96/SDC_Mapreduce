3392c157ff59b15b465a109a1d7e4b5e59549c02
Sricharan <sravi73>
2022-04-06 19:44:38 +0000

    all changes

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ cmake .
 make -j
 docker build -f docker/Dockerfile.worker -t worker_image .
 docker build -f docker/Dockerfile.master -t master_image .
-docker tag worker_image:latest srics96/worker_image:latest
-docker push srics96/worker_image:latest
+az acr login -n mapreduceimage
+docker tag worker_image:latest mapreduceimage.azurecr.io/mapreduce/worker_image
+docker push mapreduceimage.azurecr.io/mapreduce/worker_image
 
-docker tag master_image:latest srics96/master_image:latest
-docker push srics96/master_image:latest
+docker tag master_image:latest mapreduceimage.azurecr.io/mapreduce/master_image
+docker push mapreduceimage.azurecr.io/mapreduce/master_image
